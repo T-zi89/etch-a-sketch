aec3538592b9306414f126c2445313ed94517b2c
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2025-02-03 13:57:37 +0100

    set up the files

---
+++ javascript.js
@@ -0,0 +1,19 @@
+console.log("Etch-A-Sketch")
+const cointainer = document.getElementById("container");
+let grid = document.createElement("div");
+grid.classList.add("grid");
+grid.textContent = "GRID";
+
+
+function createGrid() {
+for(i = 0; i < 16; i++);
+grid = grid++
+console.log(grid)
+}
+
+
+
+
+cointainer.appendChild(grid);
+
+createGrid()
