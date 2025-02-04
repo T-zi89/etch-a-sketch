ebcd849de598bcf3c91ee1b4f299e119daa6b188
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2025-02-04 13:58:08 +0100

    create div function

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
 console.log("Etch-A-Sketch")
 const cointainer = document.getElementById("container");
-let grid = document.createElement("div");
-grid.classList.add("grid");
-grid.textContent = "GRID";
 
 
+
+let squares = window.prompt("Squares")
 function createGrid() {
-for(i = 0; i < 16; i++);
-grid = grid++
-console.log(grid)
+for(i = 0; i < squares; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.textContent = "GRID";
+    cointainer.appendChild(grid);
+}
 }
 
 
 
 
-cointainer.appendChild(grid);
 
-createGrid()
+
+createGrid(squares)
