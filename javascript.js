f0124024e14c2f09e3202e1cc2fb5530433a94a0
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2025-02-05 08:07:01 +0100

    add some css

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 console.log("Etch-A-Sketch")
 const cointainer = document.getElementById("container");
-
+const etchASketch = document.getElementById("etch-a-sketch");
 
 
 let squares = window.prompt("Squares")
 function createGrid() {
-for(i = 0; i < squares; i++) {
+for(i = 0; i < squares*squares; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     grid.textContent = "GRID";
-    cointainer.appendChild(grid);
+    etchASketch.appendChild(grid);
 }
 }
 
