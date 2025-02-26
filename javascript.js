fb64a634b93f9140149872aa0b2428f45df95e4d
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2025-02-26 13:18:20 +0100

    add the main functions to the board

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
+
 console.log("Etch-A-Sketch")
 const cointainer = document.getElementById("container");
-const canvas = document.getElementById("canvas");
+const divContainer = document.getElementById("divContainer");
+const board = document.getElementById("board");
+const newBoardBtn = document.getElementById("newBoardBtn");
+const clearBoardBtn = document.getElementById("clearBoardBtn");
+const redBtn = document.getElementById("redBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
 
-let gridDefault = 16
-
+let size = 16
+let color = ""
 
 // This function asks the user for a number of squares for the canvas
 // and creates a grid of squared divs by squaring the inputted number
@@ -11,35 +18,70 @@ let gridDefault = 16
 // the canvas by the number of squares so that it stays constant
 // e.g. if the square number is 10 and the canvas is 500px*500px
 // 500/10 = 50px for the height and 50 px for the width of every div 
-let promptNum = window.prompt("Squares")
-function createGrid(promptNum) {
-    const squareSize = 500/promptNum;
-for(i = 0; i < promptNum*promptNum; i++) {
-    const squares = document.createElement("div");
-    squares.classList.add("squares");
-    squares.style.width = `${squareSize}px`;
-    squares.style.height = `${squareSize}px`;
-    squares.textContent = "";
-    canvas.appendChild(squares);
-    squares.addEventListener("mouseover", changeColor)
+
+function createGrid(size) {
+    const squareSize = 500/((size));
+for(i = 0; i < size*size; i++) {
+    let div = document.createElement("div");
+    div.classList.add("divs")
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
+    board.appendChild(div);
+    div.addEventListener("mouseover", colorDiv)
     }
+    const allDivs = board.querySelectorAll(".divs");
 }
 
-createGrid(promptNum)
-
-canvas.style.color = "red"
-
-function alertFunction() {
-    alert("click");
+function getSize() {
+    let squaresNum = window.prompt("Please choose the number of squares for each side of the sketching board, it should be between 2 and 100.");
+    console.log(squaresNum)
+        if (squaresNum < 2 || squaresNum > 100) {
+            alert("The number of squares should be between 2 and 100, please choose again.");
+            getSize();
+        } else {
+            size = squaresNum;    
+        }
 }
 
-function changeColor() {
-    console.log("Hello World")
+// function alertFunction() {
+//     alert("click");
+// }
+
+function colorDiv() {
+if (color == "red") {
     this.style.backgroundColor = "red";
+} else if (color == "white") {
+    this.style.backgroundColor = "white";
+}}
+
+function resetBoard() { 
+    const allDivs = board.querySelectorAll(".divs");
+    allDivs.forEach((div) => div.style.backgroundColor = "white");
+}
+function removeDivs(){
+    const allDivs = board.querySelectorAll(".divs");
+    allDivs.forEach((div) => board.removeChild(div));  
+}
+    
+redBtn.addEventListener("click", () => {
+    setColor("red");
+})
+
+eraserBtn.addEventListener("click", () => {
+    setColor("white");
+});
+
+
+function setColor(colorChoice) {
+    color = colorChoice
 }
 
-const clearCanvasBtn = document.getElementById("clearCanvas")
+clearBoardBtn.addEventListener("click", resetBoard)
 
-clearCanvasBtn.addEventListener("click", () => {
-squares.style.backgroundColor = "blue";
+newBoardBtn.addEventListener("click", () => {
+    removeDivs();
+    getSize();
+    createGrid(size);
 })
+
+createGrid(size)
